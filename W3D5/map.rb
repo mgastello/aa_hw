fb15a00e89783f49c99415572e89659e991b352b
Michael Gastello <michaelgastello@aol.com>
2023-01-27 00:38:45 -0500

    added Map class

---
+++ W3D5/map.rb
@@ -0,0 +1,19 @@
+class Map
+    attr_reader :array
+
+    def initialize
+        @array = []
+    end
+
+    def set(key, value)
+
+    end
+
+    def get(key)
+
+    end
+
+    def delete(key)
+
+    end
+end
