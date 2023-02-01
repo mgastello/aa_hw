6dbc140bc3b352aa1f6b7041cb92fce211a3a81d
Michael Gastello <michaelgastello@aol.com>
2023-01-31 23:40:57 -0500

    finished hw

--- W4D3/skeleton/lib/simon.rb
+++ W4D3/skeleton/lib/simon.rb
@@ -3,25 +3,33 @@ class Simon
 
   attr_accessor :sequence_length, :game_over, :seq
 
-  def initialize(sequence_length=1)
-    @sequence_length = sequence_length
+  def initialize
+    @sequence_length = 1
     @seq = []
     @game_over = false
   end
 
   def play
+    until game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
-    if game_over == false
-      self.show_sequence
-      self.require_sequence
+    self.show_sequence
+    self.require_sequence
+
+    unless game_over
+      self.round_success_message
+      @sequence_length += 1
     end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -41,6 +49,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
