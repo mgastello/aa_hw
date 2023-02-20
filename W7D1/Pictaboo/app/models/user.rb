cbbbe742d8a5412f7b223a39bed8bdc1e4644589
Michael Gastello <michaelgastello@aol.com>
2023-02-20 01:40:22 -0500

    finished hw

---
+++ W7D1/Pictaboo/app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: "Password can't be blank" }
+    validates :password, length: { minimum: 6, allow_nil: true }
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
