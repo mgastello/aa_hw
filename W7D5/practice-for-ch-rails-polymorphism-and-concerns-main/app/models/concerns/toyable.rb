da696575dd70cc0d18a8c56f4eb82778c7e0c2d7
Michael Gastello <michaelgastello@aol.com>
2023-02-24 00:35:14 -0500

    finished hw 1

---
+++ W7D5/practice-for-ch-rails-polymorphism-and-concerns-main/app/models/concerns/toyable.rb
@@ -0,0 +1,13 @@
+module Toyable
+    extend ActiveSupport::Concern
+  
+    included do
+        has_many :toys, as: :toyable
+    end
+  
+    def receive_toy(name)
+        Toy.find_or_create_by(name: name) do |toy|
+            toy.toyable = self
+        end
+    end
+end
